7ca02540fca77d874abf37cb7086e38e8e2c73fb
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-03-28 23:41:38 +0530

    Day-2

--- Check sorted Array.cpp
+++ Check sorted Array.cpp
@@ -7,12 +7,16 @@ using namespace std;
 // } Driver Code Ends
 // User function template for C++
 
-class Solution {
-  public:
-    bool arraySortedOrNot(int arr[], int n) {
+class Solution
+{
+public:
+    bool arraySortedOrNot(int arr[], int n)
+    {
         // code here
-        for(int i = 0; i < n - 1; i++){
-            if(arr[i] > arr[i + 1]){
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
                 return false;
             }
         }
@@ -22,14 +26,17 @@ class Solution {
 
 //{ Driver Code Starts.
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
         int arr[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> arr[i];
         }
         Solution ob;
