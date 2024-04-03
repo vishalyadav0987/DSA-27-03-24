94470dd155a8c40e59a16ada089970619144c496
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-04-03 10:28:23 +0530

    Day-8

---
+++ nextGreater med.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+       vector<long long> result(n , -1);
+       vector<int> s;
+       for(int i = 0; i < n; i++){
+          while(!s.empty() && arr[i] > arr[s.back()]){
+              
+              result[s.back()] = arr[i];
+              
+              s.pop_back();
+          }
+          s.push_back(i);
+       }
+       return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
