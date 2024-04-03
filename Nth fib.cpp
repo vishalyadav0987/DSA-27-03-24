f673aeb2e469c6321ba5eef98b2aba3ef2c59bb0
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-04-03 09:27:30 +0530

    Day-8

---
+++ Nth fib.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int fib(int N){
+        //code here
+        if(N == 0) return 0;
+        if(N == 1) return 1;
+        int a = 0;
+        int b = 1;
+        int c;
+        for(int i = 2; i <= N; i++){
+            c = (a + b) % 10;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.fib(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
