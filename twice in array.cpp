a0f077716e7503566099dfc1da709c05690d6e80
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2024-04-18 09:00:51 +0530

    Day-22

---
+++ twice in array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int arr[], int n) {
+        // Your code here
+        int hash[n+3]={0};
+        vector<int>res;
+        for(int i = 0; i<n+2;i++){
+            hash[arr[i]] +=1;
+                if(hash[arr[i]] == 2){
+                res.push_back(arr[i]);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
