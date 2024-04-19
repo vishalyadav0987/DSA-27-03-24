71eeff70ad6f6611017c2d3a210de55db1a2de32
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-04-20 00:20:46 +0530

    Day-25

---
+++ Duplicate in array.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> duplicates(long long arr[], int n)
+    {
+        // code here
+        int hash[n + 1] = {0};
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            hash[arr[i]]++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (hash[i] > 1)
+            {
+                ans.push_back(i);
+            }
+        }
+        // sort(ans.begin(),ans.end());
+        if (ans.empty())
+        {
+            ans.push_back(-1);
+            return ans;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        int n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
