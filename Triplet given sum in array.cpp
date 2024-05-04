3ac5a0f4f75710898af2495837290b9a962e8e6f
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-04 08:54:35 +0530

    Day-37

---
+++ Triplet given sum in array.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to find if there exists a triplet in the
+    // array A[] which sums up to X.
+    bool find3Numbers(int arr[], int n, int X)
+    {
+        // Your Code Here
+        sort(arr, arr + n);
+        int j, k;
+        for (int i = 0; i < n - 2; i++)
+        {
+            j = i + 1;
+            k = n - 1;
+            while (j < k)
+            {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum == X)
+                {
+                    return true;
+                }
+                else if (sum < X)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n, X;
+        cin >> n >> X;
+        int i, A[n];
+        for (i = 0; i < n; i++)
+            cin >> A[i];
+        Solution ob;
+        cout << ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
