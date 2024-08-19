3b80aa9ec7729276a44ff934d892789d123e6eda
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-19 11:38:19 +0530

    Restart day 2

---
+++ Rotate array by one place.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    void rotate(vector<int> &arr)
+    {
+        // code here
+        int n = arr.size();
+        int temp = arr[arr.size() - 1];
+        for (int i = 1; i < arr.size(); i++)
+        {
+            arr[n - i] = arr[n - i - 1];
+        }
+        arr[0] = temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.rotate(arr);
+        for (long long i = 0; i < arr.size(); i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
