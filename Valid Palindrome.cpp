e4a75a4ca85231bc56504d8e2950a651bcfbb184
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-09-14 01:14:27 +0530

    Valid Palindrome

---
+++ Valid Palindrome.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <string>
+#include <cctype>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAlphanumeric(char s)
+    {
+        return (s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z') || (s >= '0' && s <= '9');
+    }
+
+    string reverse(const string &s)
+    {
+        string reversed = s;
+        int i = 0, j = s.length() - 1;
+        while (i < j)
+        {
+            swap(reversed[i], reversed[j]);
+            i++;
+            j--;
+        }
+        return reversed;
+    }
+
+    string toLowercase(const string &s)
+    {
+        string lowercase = s;
+        for (char &c : lowercase)
+        {
+            c = tolower(c);
+        }
+        return lowercase;
+    }
+
+    bool isPalindrome(const string &s)
+    {
+        string filtered;
+        for (char c : s)
+        {
+            if (isAlphanumeric(c))
+            {
+                filtered += c;
+            }
+        }
+
+        string lowercaseFiltered = toLowercase(filtered);
+        string reversedLowercaseFiltered = toLowercase(reverse(lowercaseFiltered));
+
+        return lowercaseFiltered == reversedLowercaseFiltered;
+    }
+};
