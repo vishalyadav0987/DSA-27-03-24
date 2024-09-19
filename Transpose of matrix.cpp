92fc9354a649aa7768f178c586843a4575e8d5fd
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-09-19 07:54:40 +0530

    Transpose of matrix

---
+++ Transpose of matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix)
+    {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        cout << row << " " << col << endl;
+        vector<vector<int>> ans(col, vector<int>(row));
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
