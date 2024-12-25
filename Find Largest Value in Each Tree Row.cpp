35fc88bd2a6a73ffc81d05651e5542f3d947aa22
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-12-25 16:00:21 +0530

    Find Largest Value in Each Tree Row Streak Problem Day = 86

---
+++ Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,75 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <numeric>
+#include <sstream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode 
+     right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> largestValues(TreeNode *root)
+    {
+        vector<int> ans;
+        if (!root)
+            return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size = q.size();
+            int maxi = INT_MIN;
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *front = q.front();
+                int data = front->val;
+                q.pop();
+                if (maxi <= data)
+                {
+                    maxi = data;
+                }
+                if (front->left)
+                {
+                    q.push(front->left);
+                }
+                if (front->right)
+                {
+                    q.push(front->right);
+                }
+            }
+            ans.push_back(maxi);
+        }
+        return ans;
+    }
+};
+int main()
+{
+    return 0;
+}
