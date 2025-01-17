ff3997a0bbffae826a73a48f4e5e538971f512da
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-17 23:34:10 +0530

    Add solution for Neighboring Bitwise XOR problem

---
+++ Neighboring Bitwise XOR.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <numeric>
+#include <sstream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+class Solution
+{
+public:
+    bool doesValidArrayExist(vector<int> &derived)
+    {
+        return !accumulate(derived.begin(), derived.end(), 0, bit_xor<int>());
+    }
+};
+int main()
+{
+    return 0;
+}
